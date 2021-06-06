ed67e6206ad964514cb9d917a95e40b83e8ec0ad
dgarridomarquez <dgm@uma.es>
2021-06-06 18:57:04 +0200

    dynamic filter

--- streamlit1.py
+++ streamlit1.py
@@ -11,22 +11,16 @@ df = pandas.read_csv("https://raw.githubusercontent.com/kirenz/datasets/master/g
   
 chart_visual = st.sidebar.selectbox('Seleccionar gráfica', 
                                     ('Line Chart', 'Bar Chart', 'Pie Chart'))
-#marcadores = st.sidebar.checkbox("Mostrar marcadores", True)
+
+if st.sidebar.checkbox("Mostrar datos", True):
+        st.dataframe(df)
 
 fig = go.Figure()
-#if marcadores:
-#        smarcadores = "lines+markers"
-#else:
-#        smarcadores = "lines"
-
 
 if chart_visual == 'Line Chart':
         df2 = df[df["country"] == 'Spain']
         fig.add_trace(go.Scatter(x = df2["year"], y = df2["pop"],
                                  mode = "lines"))
-
-#        fig.add_trace(go.Scatter(x = df2["year"], y = df2["pop"],
-#                                 mode = smarcadores))
 
         fig.update_layout(title='Evolución Población España',
                    xaxis_title='Año',
@@ -35,8 +29,10 @@ if chart_visual == 'Line Chart':
 
 
 if chart_visual == 'Bar Chart':
-        df2 = df[df["year"] == 2007]
-        fig.add_trace(go.Bar(x = df2.iloc[0:10].country, y = df2.iloc[0:10].lifeExp))
+        year = st.slider('year',2007,1952,2007)
+        df2 = df[df["year"] == year]
+        fig.add_trace(go.Bar(x = df2.iloc[0:10].country, y = df2.iloc[0:10].lifeExp, text = df2.iloc[0:10].lifeExp,
+                textposition='auto'))
         fig.update_layout(title='Expectativa de vida en años')
         st.plotly_chart(fig, use_container_width=True)
 
